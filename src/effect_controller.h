caef7bbda0becc3b6ccd8518b41a1a4f2409567a
Max Maisel <max.maisel@posteo.de>
2025-10-01 14:50:55 +0200

    Prepare EffectController refactoring.

--- src/effect_controller.h
+++ src/effect_controller.h
@@ -1,26 +1,38 @@
-/**********************************************************************\
- * Keyboard
- *
- * Light effect controller
- **********************************************************************
- * Copyright (C) 2019-2022 - Max Maisel
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-\**********************************************************************/
+/******************************************************************************\
+    Split Keyboard
+    Copyright (C) 2019-2025 - Max Maisel
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\******************************************************************************/
 #pragma once
 
 #include "types.h"
+
+class Effect {
+    public:
+        virtual void run() = 0;
+};
+
+class EffectNone : public Effect {
+    public:
+        void run();
+};
+
+class EffectFlash : public Effect {
+    public:
+        void run();
+};
 
 class EffectController {
     // Static class
@@ -30,14 +42,13 @@ class EffectController {
     ~EffectController() = delete;
 
     public:
-        enum Effect {
-            NONE,
-            FLASH
-        };
-
-        static void set_effect(Effect effect);
-        static void on_update_page(const BYTE* old_page, const BYTE* new_page);
+        static inline void set_effect(Effect* effect) { _effect = effect; }
+        static void on_change(DWORD keys);
 
     private:
-        static Effect m_active_effect;
+        static Effect* _effect;
+        static DWORD _keys;
 };
+
+extern EffectNone effect_none;
+extern EffectFlash effect_flash;
